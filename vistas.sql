b0e9555dd9632dd01136cf5a51f5011fc537ef2e
padreariandel <hernandezjc1213@gmail.com>
2017-11-19 17:06:51 -0600

    vistas 2 en total

---
+++ vistas.sql
@@ -0,0 +1,11 @@
+
+/* VISTAS DEL PROGRAMA (2) EN TOTAL */
+CREATE VIEW vista_crud_equipo
+AS (SELECT * FROM equipo);
+
+
+CREATE VIEW vista_crud_partido
+AS (SELECT * FROM partido);
+SELECT * FROM vista_crud_equipo;
+
+
