ccf2ab3c5e746f17af5a2819aa08564617f8d8e2
reyesm2 <reyes.marlon57@gmail.com>
2017-11-19 22:35:39 -0600

    TRIGGERS-INDICES

--- createSinTIME.sql
+++ createSinTIME.sql
@@ -362,4 +362,31 @@ CREATE VIEW vista_crud_partido
 AS (SELECT * FROM partido);
 SELECT * FROM vista_crud_equipo;
 
+/* TRIGGERS DEL PROGRAMA 2 TOMADOS*/
+
+CREATE OR REPLACE TRIGGER ELIMINAR_JUGADOR_CAMBIO_LOCAL
+AFTER INSERT ON CAMBIOS_EQUIPO_LOCAL
+BEGIN
+    DELETE FROM pantilla_titular_equipo_local
+        WHERE numero_pasaporte_jugador=new.numero_pasaporte_jugador;
+
+    INSERT INTO pantilla_titular_equipo_local
+        values(new.numero_partido,new.numero_pasaporte_jugador_entra,new.codigo_equipo);
+END;
+
+CREATE OR REPLACE TRIGGER ELIMINAR_JUGADOR_CAMBIO_VISITA
+AFTER INSERT ON CAMBIOS_EQUIPO_VISITA
+BEGIN
+   DELETE FROM pantilla_titular_equipo_visita
+      WHERE numero_pasaporte_jugador=new.numero_pasaporte_jugador;
+
+   INSERT INTO pantilla_titular_equipo_visita
+        values(new.numero_partido,new.numero_pasaporte_jugador_entra,new.codigo_equipo);
+END;
+
+/*INDICES MAS NECESARIOS*/
+CREATE INDEX TABLA_JUGADORES ON JUGADOR (NUMERO_PASAPORTE ASC, CODIGO_EQUIPO ASC);
+
+CREATE INDEX TABLA_JUGADORES ON PARTIDO (NUMERO_PARTIDO ASC, FASE DESC);
+
 
---
+++ trigger
@@ -0,0 +1,22 @@
+
+
+CREATE OR REPLACE TRIGGER ELIMINAR_JUGADOR_CAMBIO_LOCAL
+AFTER INSERT ON CAMBIOS_EQUIPO_LOCAL
+BEGIN
+    DELETE FROM pantilla_titular_equipo_local
+        WHERE numero_pasaporte_jugador=new.numero_pasaporte_jugador;
+
+    INSERT INTO pantilla_titular_equipo_local
+        values(new.numero_partido,new.numero_pasaporte_jugador_entra,new.codigo_equipo);
+END;
+
+
+CREATE OR REPLACE TRIGGER ELIMINAR_JUGADOR_CAMBIO_VISITA
+AFTER INSERT ON CAMBIOS_EQUIPO_VISITA
+BEGIN
+    DELETE FROM pantilla_titular_equipo_visita
+        WHERE numero_pasaporte_jugador=new.numero_pasaporte_jugador;
+
+    INSERT INTO pantilla_titular_equipo_visita
+        values(new.numero_partido,new.numero_pasaporte_jugador_entra,new.codigo_equipo);
+END;
