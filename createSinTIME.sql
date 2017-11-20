89e236a18740a785aeea3bf68c19cd90d57a0fc9
padreariandel <hernandezjc1213@gmail.com>
2017-11-19 18:31:27 -0600

    vistas agregadas al SinTIME

--- createSinTIME.sql
+++ createSinTIME.sql
@@ -346,3 +346,14 @@ begin
             dbms_output.put_line('Please rescue me');
             raise;
 end borrar;
+
+/* VISTAS DEL PROGRAMA (2) EN TOTAL */
+CREATE VIEW vista_crud_equipo
+AS (SELECT * FROM equipo);
+
+
+CREATE VIEW vista_crud_partido
+AS (SELECT * FROM partido);
+SELECT * FROM vista_crud_equipo;
+
+
